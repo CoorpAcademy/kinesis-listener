6cb7fdae698cd4796746e75fc818f25b5cb2a625
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-21 17:00:20 +0200

    fix error typo dsk and add code to get shard

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const AWS = require('aws-dsk')
-
+const AWS = require('aws-sdk');
 
 const kinesis = new AWS.Kinesis({apiVersion: '2013-12-02'});
+
+const params = {
+  // ShardId: 'STRING_VALUE', /* required */
+  ShardIteratorType: 'LATEST',
+  StreamName: 'bricklane-central-development'
+};
+kinesis.getShardIterator(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
