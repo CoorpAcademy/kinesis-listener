c9d74c83e310d3cf4dbf7e8156ba6a887a203452
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-23 16:28:25 +0200

    prototype of a speed sinner to have an idea of the message receiption

--- cli-view.js
+++ cli-view.js
@@ -2,6 +2,10 @@ const _ = require('lodash');
 const c = require('chalk');
 const indentString = require('indent-string');
 const util = require('util');
+const cliSpinners = require('cli-spinners');
+
+const timeSpinner = cliSpinners.dots; // later option
+const speedSpinner = cliSpinners.arrow3;
 
 const view = state =>
 `${
@@ -9,7 +13,8 @@ c.bold(`${c.red('►')} Listening ${c.blue.underline(state.kinesisStream)} kines
   - stream with ${c.blue.bold(state.shardsIds.length) } shards: ${c.dim.grey(state.shardsIds.join(', '))}
   - received so far ${c.blue.bold(state.count || 0)} records
 ${!state.count ? '':
-`  - last received record (at ${c.dim.grey(state.timestampLastReceived)}) :
+`  - Speed Estimation: ${c.bold.yellow(speedSpinner.frames[state.count % speedSpinner.frames.length])}
+  - last received record (at ${c.dim.grey(state.timestampLastReceived)}) :
 ${indentString(util.inspect(_.omit(state.lastJsonRecord, ['content']), {depth: null, colors: true}), 4)}`}`;
 
 module.exports = view
