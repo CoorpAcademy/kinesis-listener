81f05144b9ac5f923d6153faae6d9671c96c7780
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-25 15:49:34 +0200

    factorise spinning and add a spinner for the file

--- cli-view.js
+++ cli-view.js
@@ -7,15 +7,17 @@ const cliSpinners = require('cli-spinners');
 const timeSpinner = cliSpinners.dots; // later option
 const speedSpinner = cliSpinners.arrow3;
 
+const timeBasedSpin = (spinner, speedFactor = 1) =>
+    spinner.frames[Math.floor(new Date().getTime() / spinner.interval * speedFactor) % spinner.frames.length];
+
 const view = state =>
-`${
-c.bold(`${c.red(timeSpinner.frames[Math.floor(new Date().getTime() / timeSpinner.interval) % timeSpinner.frames.length])
-} Listening ${c.blue.underline(state.kinesisStream)} kinesis`)}
+`${c.bold(`${c.red(timeBasedSpin(timeSpinner))} Listening ${c.blue.underline(state.kinesisStream)} kinesis`)}
   - stream with ${c.blue.bold(state.shardsIds.length) } shards: ${c.dim.grey(state.shardsIds.join(', '))}
   - received so far ${c.blue.bold(state.count)} records (${
     c.grey.dim(_.map(state.shardsIds, si => state.shardCount[si] || 0).join(', '))})
 ${!state.fileStreaming ? '':
-`  - ${state.fileStreamingMessage} ${state.fileStreamingFile? c.dim.underline(state.fileStreamingFile):''}
+`  - ${state.fileStreamingMessage} ${c.cyan(timeBasedSpin(timeSpinner, 0.5))
+} ${state.fileStreamingFile? c.dim.underline(state.fileStreamingFile):''}
 `}${!state.count ? '':
 `  - Speed Estimation: ${c.bold.yellow(speedSpinner.frames[state.count % speedSpinner.frames.length])}
   - last received record (at ${c.dim.grey(state.timestampLastReceived)}) :
