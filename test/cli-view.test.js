62e6f0505f673580ed0d8c1ac6ef7e27fa16e715
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-30 14:07:03 +0200

    tests and placeholder for the views

---
+++ test/cli-view.test.js
@@ -0,0 +1,16 @@
+const test = require('ava');
+
+const {checkpoint, view} = require('../lib/cli-view');
+
+
+test('basic checkpoint with no record', t => {
+  const checkpointView = checkpoint({});
+  t.true(/Checkpoint at .*: No record received so far/.test(checkpointView) )
+});
+
+test.todo('check checkpoint with no new record');
+test.todo('checkpoint with some new record');
+
+test.todo('view with no record');
+test.todo('view with some record');
+test.todo('view with file streaming');
