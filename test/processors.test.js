9eb73d723656a0ba35ae10fc86d36df5d2c1f9ce
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-30 13:20:43 +0200

    write two simple tests for the processors counter

---
+++ test/processors.test.js
@@ -0,0 +1,28 @@
+const test = require('ava');
+
+const {counterProcessor} = require('../processors');
+
+const emptyRecord = {};
+const context = {ShardId: 'sha1'};
+
+test('counterProcessor increments count', t => {
+  const state = {count: 3, shardCount: {sha1: 3}};
+  const processor = counterProcessor(state, context);
+
+  processor(emptyRecord)
+  t.deepEqual(state.count, 4)
+  t.deepEqual(state.shardCount.sha1, 4)
+
+  processor(emptyRecord)
+  t.deepEqual(state.count, 5)
+  t.deepEqual(state.shardCount.sha1, 5)
+});
+
+test('counterProcessor start increment when no count', t => {
+  const state = {shardCount:{}}
+  const processor = counterProcessor(state, context);
+
+  processor(emptyRecord)
+  t.deepEqual(state.count, 1)
+  t.deepEqual(state.shardCount.sha1, 1)
+});
