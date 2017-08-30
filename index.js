61e0994cbc0d373394358a371cac0dbf3694211c
Adriean Khisbe <adriean.khisbe@live.fr>
2017-08-30 15:04:27 +0200

    draft of extraction of a kinesis-listener lib #wip

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const dateFormat = argv['time-format'] || (argv.d && 'hh:mm:ss');
 
 const STATE = {kinesisStream, batchSize, dateFormat, count: 0, shardCount: []}
 
+const {resilientListener} = require('./lib/kinesis-listener')(kinesis, STATE)
 
 if (argv.filename || argv.forward) {
     const file = argv.filename || '/tmp/kinesis-listener.log';
@@ -68,63 +69,7 @@ if (argv.retro) {
     ShardIteratorType = 'TRIM_HORIZON';
 }
 
-const readIterator = recordProcessors => ({ShardId, ShardIterator}) => {
-    return kinesis.getRecordsP({ShardIterator, Limit: batchSize})
-        .then(data => {
-            const iterator = data.NextShardIterator;
-            const context = {
-                ShardId, ShardIterator, nRecords: data.Records.length
-            };
-            // TODO: see MillisBehind Latest to determine batchsize?
-            const records = _.map(data.Records,
-                record => Object.assign(record, {Data: new Buffer(record.Data, 'base64').toString('utf-8')}));
-            _.map(recordProcessors, recordProcessor => _.map(records, recordProcessor(STATE, context)));
-            // maybe: later async record processor
-            return {ShardId, ShardIterator: iterator};
-        })
-};
-
-const getStreamShards = (kinesisStream) => kinesis.describeStreamP({StreamName: kinesisStream})
-    .then(streamConf => {
-        const shardsIds = _.map(streamConf.StreamDescription.Shards, 'ShardId');
-        STATE.shardsIds = shardsIds;
-        return shardsIds;
-    });
-
-const launchListener = () => getStreamShards(kinesisStream)
-    .map(shardId => kinesis.getShardIteratorP({
-        StreamName: kinesisStream,
-        ShardId: shardId, // TODO: type later configurable -> X minutes ago
-        ShardIteratorType,
-        Timestamp
-    }).then(si => ({ShardId: shardId, ShardIterator: si.ShardIterator})))
-    .then((shardIterators) => {
-        const kinesisIterator = readIterator(processorsList);
-        const readLoop = (initialIterators) => {
-            // TODO graceful STOP
-            return Promise.map(initialIterators, kinesisIterator)
-                .then(readLoop)
-        }
-        const printLoop = () => {
-            logUpdate(cliView.view(STATE));
-            return Promise.delay(updateRate).then(printLoop);
-        }
-
-        return Promise.all([readLoop(shardIterators), printLoop()]);
-    });
-
-const resilientListener = () =>
-    launchListener().catch(
-        err => _.includes(['ProvisionedThroughputExceededException', 'ExpiredIteratorException'], err.code),
-        err => {
-            logUpdate.clear()
-            console.log(c.red.bold('Error Occured'));
-            console.log(err.message);
-            console.log(c.blue.bold('Relaunching listener'));
-            return resilientListener()
-        });
-
-const main = () => resilientListener()
+const main = () => resilientListener(kinesisStream)
     .catch(err => err.name === "ResourceNotFoundException", err => {
         console.log(err.message);
         process.exit(2);
