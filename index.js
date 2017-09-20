a1326dc38f52fc44553d02a65f66ff458457c43c
Adriean Khisbe <adriean.khisbe@live.fr>
2017-09-20 16:55:04 +0200

    configurable fetch-interval, default 100ms

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ const argv = require('yargs')
   .describe('retro', 'Start to read "00h11m2s" time ago').alias('r', 'retro').string('r')
   .describe('horizon', 'Trim Horizon read').alias('H', 'horizon').boolean('H')
   .describe('refresh-rate', 'Refresh rate of the dashboard, in time per second (default 10)')
-  .alias('R', 'refresh-rate').string('R')
+  .alias('R', 'refresh-rate').number('R')
   .describe('batch-size', 'Size of batch for each kinesis getRecord (default 100)').alias('b', 'batch-size')
   .describe('time-format', 'Format to print date with').alias('t', 'time-format').string('t')
   .describe('day-format', 'Use hh:mm:ss day date format').alias('d', 'day-format').boolean('d')
+  .describe('fetch-interval', 'fetch-rate of kinesis records in ms').alias('i', 'fetch-interval').number('1')
   .number(['refresh-rate', 'batch-size'])
   .help('h').alias('h', 'help')
   .argv;
@@ -43,7 +44,7 @@ settings.config = {
   dateFormat: argv['time-format'] || (argv.d && 'hh:mm:ss'),
   batchSize: argv['batch-size'] || 100,
   updateRate: 1000 / (argv['refresh-rate'] || 10),
-  fetchInterval: 200,
+  fetchInterval: argv['fetch-interval'] || 100,
   processorsList: [...processors.BASICS]
 };
 
